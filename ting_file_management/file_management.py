7cf17ff2d54116c14a904a609154eb8916ed5019
Lucasteisouza <lucasteisouza@gmail.com>
2023-06-14 16:45:54 -0300

    feat: implementa o leitor de txt

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if path_file[-4:] != '.txt':
+        print("Formato inválido", file=sys.stderr)
+        # raise ValueError("Formato inválido")
+    try:
+        with open(path_file, 'r') as file:
+            return file.read().splitlines()
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+        # raise FileNotFoundError(f"Arquivo {path_file} não encontrado")
