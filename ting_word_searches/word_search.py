1d86b6b2a48f05da02c1032f7e5fe2c9ea3a926e
Lucasteisouza <lucasteisouza@gmail.com>
2023-06-15 08:40:31 -0300

    feat: implementa funcao para verificar existencia de uma palavra nos elementos da fila

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,5 +1,18 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    output_list = []
+    for i in range(0, len(instance)):
+        search_result = instance.search(i)
+        entry_ocurrances = {
+            "palavra": word,
+            "arquivo": search_result["nome_do_arquivo"],
+            "ocorrencias": []
+        }
+        for j in range(0, len(search_result["linhas_do_arquivo"])):
+            if word.lower() in search_result["linhas_do_arquivo"][j].lower():
+                entry_ocurrances["ocorrencias"].append({"linha": j + 1})
+        if len(entry_ocurrances["ocorrencias"]) > 0:
+            output_list.append(entry_ocurrances)
+    return output_list
 
 
 def search_by_word(word, instance):
